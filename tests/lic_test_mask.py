4a983dfe4119f80eb0991f7822616b817287508c
Bhanu Tokas <bhanu.dharam.tokas@gmail.com>
2025-02-23 23:27:30 -0700

    Added Mask Tests

---
+++ tests/lic_test_mask.py
@@ -0,0 +1,233 @@
+import os
+import sys
+import torch
+import random
+import argparse
+import numpy as np
+from LIC import LIC
+import pandas as pd
+from utils.text import CaptionProcessor
+from utils.altcreator import MaskCaptionGenderDataset
+from attackerModels.NetModel import LSTM_ANN_Model
+
+torch.backends.cudnn.deterministic = True
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+print("GPU Available:", torch.cuda.is_available())
+# Print the currently active GPU
+if torch.cuda.is_available():
+    print("Current GPU:", torch.cuda.get_device_name(torch.cuda.current_device()))
+    print("GPU Device Index:", torch.cuda.current_device())
+    print("Total GPUs:", torch.cuda.device_count())
+
+# Define thresholds for contextual LIC
+contextual_thresholds = [round(x * 0.05, 2) for x in range(11, 16)]
+
+# Step 1: Define Gender Words and Token
+masculine = [
+    "man",
+    "men",
+    "male",
+    "father",
+    "gentleman",
+    "gentlemen",
+    "boy",
+    "boys",
+    "uncle",
+    "husband",
+    "actor",
+    "prince",
+    "waiter",
+    "son",
+    "he",
+    "his",
+    "him",
+    "himself",
+    "brother",
+    "brothers",
+    "guy",
+    "guys",
+    "emperor",
+    "emperors",
+    "dude",
+    "dudes",
+    "cowboy",
+]
+feminine = [
+    "woman",
+    "women",
+    "female",
+    "lady",
+    "ladies",
+    "mother",
+    "girl",
+    "girls",
+    "aunt",
+    "wife",
+    "actress",
+    "princess",
+    "waitress",
+    "daughter",
+    "she",
+    "her",
+    "hers",
+    "herself",
+    "sister",
+    "sisters",
+    "queen",
+    "queens",
+    "pregnant",
+]
+gender_words = masculine + feminine
+gender_token = "gender"
+
+
+def calculate_lic(data_obj, processor, lic_model, mode="non-contextual", threshold=0.5):
+    print(
+        f"\nCalculating LIC for mode: {mode}, Threshold: {threshold if threshold else 'N/A'}"
+    )
+
+    combined_data = data_obj.getDataCombined()
+    print("\nLoaded Combined Dataset:")
+    print(f"Total Samples: {len(combined_data)}")
+
+    # Extract Features
+    human_ann = combined_data["caption_human"]
+    model_ann = combined_data["caption_model"]
+    feat = torch.tensor(
+        combined_data["gender"].values, dtype=torch.float, device=device
+    ).reshape(-1, 1)
+    feat = torch.hstack([feat, 1 - feat])
+
+    print("\nPreprocessing Captions...")
+
+    # Calculate LIC Score
+
+    lic_score = lic_model.getAmortizedLeakage(
+        feat, human_ann, model_ann, normalized=False
+    )
+    print(f"\nLIC Score for mode {mode}, Threshold {threshold}: {lic_score}")
+    return lic_score
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Test LIC and Contextual LIC calculations"
+    )
+    parser.add_argument(
+        "--human_path", required=True, help="Path to human annotations pickle file"
+    )
+    parser.add_argument(
+        "--model_path", required=True, help="Path to model annotations pickle file"
+    )
+    parser.add_argument(
+        "--glove_path",
+        required=True,
+        help="Path to GloVe embeddings in word2vec format",
+    )
+    parser.add_argument(
+        "--output_file", default="lic_scores.csv", help="Output file to save LIC scores"
+    )
+    parser.add_argument(
+        "--mode",
+        required=True,
+        choices=["contextual", "non-contextual"],
+        help="Choose mode: 'contextual' or 'non-contextual'",
+    )
+    parser.add_argument(
+        "--seed",
+        default=0,
+        help="Set random seed for the experiment. Helps ensure reproducability.",
+    )
+    args = parser.parse_args()
+
+    # Setting random seed
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+
+    # Initialize objects
+    data_obj = MaskCaptionGenderDataset(args.human_path, args.model_path)
+    processor = CaptionProcessor(
+        gender_words=gender_words,
+        obj_words=[],
+        glove_path=args.glove_path,
+        tokenizer="nltk",
+        gender_token=gender_token,
+    )
+
+    # Initialize LIC
+    lic_model = LIC(
+        model_params={
+            "attacker_class": LSTM_ANN_Model,
+            "attacker_params": {
+                "embedding_dim": 250,
+                "pad_idx": 0,
+                "lstm_hidden_size": 256,
+                "lstm_num_layers": 2,
+                "lstm_bidirectional": True,
+                "ann_output_size": 2,
+                "num_ann_layers": 2,
+                "ann_numFirst": 64,
+            },
+        },
+        train_params={
+            "learning_rate": 0.01,
+            "loss_function": "bce",
+            "epochs": 50,
+            "batch_size": 1024,
+        },
+        gender_words=gender_words,
+        obj_words=[],
+        gender_token=gender_token,
+        obj_token="obj",
+        glove_path=args.glove_path,
+        device=device,
+        eval_metric="bce",
+    )
+
+    # Initialize results storage
+    results = []
+    # Calculate LIC based on selected mode
+    if args.mode == "non-contextual":
+        non_contextual_lic = calculate_lic(
+            data_obj, processor, lic_model, mode="non-contextual"
+        )
+        results.append(
+            {
+                "mode": "non-contextual",
+                "threshold": "N/A",
+                "lic_score_mean": non_contextual_lic["Mean"].item(),
+                "lic_score_std_dev": non_contextual_lic["std"].item(),
+                "num_trials": non_contextual_lic["num_trials"],
+            }
+        )
+
+    elif args.mode == "contextual":
+        for threshold in contextual_thresholds:
+            contextual_lic = calculate_lic(
+                data_obj, processor, lic_model, mode="contextual", threshold=threshold
+            )
+            results.append(
+                {
+                    "mode": "contextual",
+                    "threshold": threshold,
+                    "lic_score_mean": contextual_lic["Mean"].item(),
+                    "lic_score_std_dev": contextual_lic["std"].item(),
+                    "num_trials": contextual_lic["num_trials"],
+                }
+            )
+
+    # Save results to CSV
+    output_dir = "/".join(args.output_file.split("/")[:-1])
+    if not (os.path.isdir(output_dir)):
+        os.makedirs(output_dir)
+    results_df = pd.DataFrame(results)
+    results_df.to_csv(args.output_file, index=False)
+    print(f"\nResults saved to {args.output_file}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils/altcreator.py
@@ -0,0 +1,117 @@
+# Importing Libraries
+import os
+import json
+import nltk
+import pickle
+import numpy as np
+import pandas as pd
+from typing import Union, Literal
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.preprocessing import MultiLabelBinarizer
+
+nltk.download("wordnet")
+pathType = Union[str, os.PathLike]
+boolNum = Literal[0, 1]
+
+
+# Helper Function
+def checkWordPresence(word: str, sentence: str) -> int:
+    return 1 if word in sentence.split(" ") else 0
+
+
+# Data Class
+class MaskCaptionGenderDataset:
+    def __init__(self, human_ann_file: pathType, model_ann_file: pathType) -> None:
+        if not os.path.exists(human_ann_file) or not os.path.exists(model_ann_file):
+            raise FileNotFoundError("Input files are missing.")
+
+        self.human_ann_path = human_ann_file
+        self.model_ann_path = model_ann_file
+        print("Reading Annotation Files")
+        self.human_data = self.read_pkl_file(self.human_ann_path)
+        with open(model_ann_file, "r") as f:
+            self.model_data = json.load(f)
+        self.wnl = WordNetLemmatizer()
+        print("Processing Annotation Data")
+        self.processData()
+
+    @staticmethod
+    def read_pkl_file(file_path: pathType) -> list[dict]:
+        with open(file_path, "rb") as f:
+            return pickle.load(f)
+
+    def processData(self) -> None:
+        self.mlb = MultiLabelBinarizer()
+        self.human_ann = {"img_id": [], "caption": []}
+        self.model_ann = {"img_id": [], "caption": []}
+        self.attribute_data = {"img_id": [], "gender": [], "objects": []}
+
+        for item in self.human_data:
+            self.attribute_data["img_id"].append(item["img_id"])
+            self.attribute_data["gender"].append(item["bb_gender"])
+            self.attribute_data["objects"].append(item["rmdup_object_list"])
+            self.human_ann["img_id"].extend(
+                [item["img_id"]] * len(item["caption_list"])
+            )
+            self.human_ann["caption"].extend(item["caption_list"])
+
+        for item in self.model_data:
+            self.model_ann["img_id"].append(int(item["image_id"].split(".")[0]))
+            self.model_ann["caption"].append(item["caption"])
+
+        self.human_ann = pd.DataFrame(self.human_ann)
+        self.model_ann = pd.DataFrame(self.model_ann)
+        self.attribute_data = pd.DataFrame(self.attribute_data)
+
+        objs = self.mlb.fit_transform(self.attribute_data["objects"])
+        self.object_presence_df = pd.DataFrame(
+            objs, columns=self.mlb.classes_, index=self.attribute_data["img_id"]
+        )
+        self.object_presence_df.fillna(0, inplace=True)
+        self.attribute_data["gender"] = self.attribute_data["gender"].apply(
+            lambda x: 1 if x == "Male" else 0
+        )
+
+    def getData(self) -> list[pd.DataFrame]:
+        human_merged = self.human_ann.merge(
+            self.attribute_data.drop("objects", axis=1), on="img_id", how="left"
+        )
+        model_merged = self.model_ann.merge(
+            self.attribute_data.drop("objects", axis=1), on="img_id", how="left"
+        )
+        return human_merged, model_merged
+
+    def get_object_presence_df(self) -> pd.DataFrame:
+        return self.object_presence_df
+
+    def getDataCombined(self) -> pd.DataFrame:
+        human_merged, model_merged = self.getData()
+        return human_merged.merge(
+            model_merged[["img_id", "caption"]],
+            on="img_id",
+            suffixes=["_human", "_model"],
+        )
+
+    def getLabelPresence(self, labels: list[str], captions: pd.Series) -> pd.DataFrame:
+        new_labels = [self.wnl.lemmatize(label) for label in labels]
+        new_captions = captions.apply(
+            lambda x: " ".join([self.wnl.lemmatize(item) for item in x.split(" ")])
+        )
+        presence_df = pd.DataFrame({"caption": captions})
+        for label in new_labels:
+            presence_df[label] = new_captions.apply(
+                lambda sentence: checkWordPresence(label, sentence)
+            )
+        return presence_df
+
+
+if __name__ == "__main__":
+    HUMAN_ANN_PATH = "./bias_data/Human_Ann/gender_obj_cap_mw_entries.pkl"
+    MODEL_ANN_PATH = "./data/captions_fc/fc_no_masking.json"
+    data_obj = MaskCaptionGenderDataset(HUMAN_ANN_PATH, MODEL_ANN_PATH)
+    human_ann, model_ann = data_obj.getData()
+    object_presence_df = data_obj.get_object_presence_df()
+
+    print("Human Annotations Sample:\n", human_ann.head())
+    print("Model Annotations Sample:\n", model_ann.head())
+    print("Object Presence DataFrame:\n", object_presence_df.head())
